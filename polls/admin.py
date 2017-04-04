1988a71d1e140a7670dbf23d32c1a9a85a5dd06a
VTB <VTB>
2017-04-04 11:14:55 +0530

    first commit

---
+++ polls/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from polls.models import Question, todo
+# Register your models here.
+admin.site.register(Question)
+admin.site.register(todo)
---
+++ polls/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from polls.models import todo
+
+
+# Create your views here.
+def index(request):
+    ret={}
+    ret['name']=request.GET.get('name')
+    #ret['length']=range(len(ret['name']))
+   
+    print(ret)
+    print(request)
+    return render(request,'login.html',ret)
+def list_todo(request):
+    ret={}
+    ret['todo']=todo.objects.all()
+    return render(request,'todo.html',ret)
+
+    
+
+
+    
